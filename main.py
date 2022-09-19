fb350f6c54f555bbcf2512d1f0a6660fbb8d816a
David Le <104813594+ledavid0106@users.noreply.github.com>
2022-09-19 11:29:02 -0700

    created more functions and flow to the story

--- main.py
+++ main.py
@@ -7,13 +7,43 @@
 # As a developer, I want to use an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.  
 # As a developer, I want my RunGame() function to call my other functions in a logical order that will determine game flow. 
 # As a developer, I want all of my functions to have a Single Responsibility. Remember, each function should do just one thing!  
-herk = {"health": 100, "attack_power": 25}
-print(herk)
-
-
+hercules = {"health": 101, "attack_power": 25}
+hydra = {"health": 300, "attack_power": 50}
+her_moves = {"Hype": +25, "Slash": 50, "Take Aim": 80, "Thunderbolt": 100}
+hyd_moves = {"Dust Storm": -0, "Crunch": 30, "Ancient Power": 40, "Hyper Beam": 100}
+enemy = "Hydra"
+import random
+def intro():
+    intros = input("Would you like a review on how attacks and power dynamics work? Y or N ")
+    if intros == "Y":
+        print("Each entity has a base amount of health and attack power. They will also have 4 different 'moves' that they can perform! Some will do damage while others can provide buffs. For the 'moves' that can do damage, the base attack power will be added to the number that follows the 'move' and will inflict # amount of damage to the opponent.")
+        print("Let's get started!")
+    else:
+        print("Let's get started!")    
+def her_status():
+    print(f"Hercules currently has Health: {hercules['health']} and Attack Power: {hercules['attack_power']}")
+def hyd_status():
+    print(f"The {enemy} has Health: {hydra['health']} and Attack Power: {hydra['attack_power']}")    
 def starting():
     print("Welcome to the world of the Greek Gods! A world full of wonders and mysteries but also leeks danger. ")
 def herc():
     print("Here we are witnessing our Hero Hercules facing one of his many trials to defeat his enemy but he needs your help! ")
-    print(f"Let's take a look at how our Hero is doing. Hercules currently has Health: {herk['health']} and Attack Power: {herk['attack_power']}")
-herc()
+    print(f"Let's take a look at how our Hero is doing. ")
+def hyd():
+    print("Seems like the monster our Hero is fighting is the 9 Headed Lernaean Hydra!")
+def menu(moves):
+    move = input(f"What move would you like to use? {(moves)} ")
+
+def enemy(moves):
+    return random.choice(moves)
+def log():
+    
+    print(f"Hercules has used {menu}! {enemy} now has ")
+    hea = hercules.get("health")
+    # hydra.get("health") -  
+    result = (hydr - hea)
+    print(result)
+    # print(f"Hercules has used {menu}!")
+log()
+ 
+print(hercules.get("attack_power"))
---
+++ tests.py
@@ -0,0 +1,7 @@
+hercules = {"health": 101, "attack_power": 25}
+hydra = {"health": 300, "attack_power": 50}
+her_moves = {"Hype": +25, "Slash": 50, "Take Aim": 80, "Thunderbolt": 100}
+hyd_moves = {"Dust Storm": -0, "Crunch": 30, "Ancient Power": 40, "Hyper Beam": 100}
+enemy = "Hydra"
+import random
+print(hercules.get("attack_power"))
